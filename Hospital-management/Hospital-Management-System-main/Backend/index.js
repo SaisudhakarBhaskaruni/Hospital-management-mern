e6cc1c62610f3200df1f372e5f60d1f4d10c944d
Leelasai <99793661+SaisudhakarBhaskaruni@users.noreply.github.com>
2024-07-27 00:26:14 +0530

    Add files via upload
    This is a project base on hospital-management-system which we can This project is based on a hospital management system that allows users to search for hospitals and perform CRUD (Create, Read, Update, Delete) operations. It features both UI and UX design elements to enhance user experience. Users can utilize GET, POST, PUT, and DELETE methods to interact with the system's data.each the hospital and get ,post,push,delete methods and as well .

---
+++ Hospital-management/Hospital-Management-System-main/Backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const hospitalRoutes = require('./routes/hospitalRoutes');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+        origin: ['http://localhost:5173','https://hms-mern.netlify.app'],
+        credentials: true
+}));
+
+app.use('/api/v1/hospitals', hospitalRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Hospital Management API');
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
